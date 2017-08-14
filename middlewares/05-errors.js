32b41b0b3a5608395669e2d8d3942359abbaaa3e
Be-Smart <serhii.vladymyrov@gmail.com>
2017-08-14 22:56:08 +0300

    Handle mongoose's validation error in the err middleware

--- middlewares/05-errors.js
+++ middlewares/05-errors.js
@@ -1,13 +1,23 @@
-
-
 module.exports = async (ctx, next) => {
   try {
     await next();
   } catch (e) {
+    const preferredType = ctx.accepts('html', 'json');
+
     if (e.status) {
       // could use template methods to render error page
       ctx.body = e.message;
       ctx.status = e.status;
+    } else if (e.name === 'ValidationError') {
+      ctx.status = 400;
+
+      const errors = {};
+
+      Object.keys(e.errors).forEach((field) => {
+        errors[field] = e.errors[field];
+      });
+
+      ctx.body = preferredType === 'json' ? { errors } : `Error: ${errors.title.message}`;
     } else {
       ctx.body = 'Error 500';
       ctx.status = 500;
